1f2add19e50aabe574c682804e7521abce4c92e2
Awee-pixel <arhatnusipbai002@gmail.com>
2026-01-28 22:18:55 +0500

    Add Practice1 Python basics examples

---
+++ practice1/data_types.py
@@ -0,0 +1,22 @@
+x="Hello, World!"
+y=20
+#z is float
+z=10.5
+print(x,y,z)
+print(type(x))
+print(type(y))
+print(type(z))
+
+#d is just string,while e is the list(or map in c++)
+d=str("Hello world")
+e=list(("Apple","Banana","Coconut"))
+print(d)
+print(e)
+#set the set n ascending order and remove copies
+j=set(("Apple","Apple","Banana"))
+print(j)
+#bytes a little interesting
+w=bytes(5)
+print(w)
+q=bytearray(5) #whats difference between butes and bytearray,is second one just list of bytes?i think so
+print(q)
